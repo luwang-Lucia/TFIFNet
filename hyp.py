19f3675229419bb4ce945e73af6a14c78ee61b11
Lu Wang <60863700+luwang-Lucia@users.noreply.github.com>
2025-11-18 15:39:45 +0800

    Add files via upload

---
+++ hyp.py
@@ -0,0 +1,19 @@
+# learning rate
+lr0: 0.0001
+warmup_lr: 0.00001
+warm_epoch: 10  # 10
+
+# setting
+num_classes: 1
+
+# training
+epochs: 100
+batch_size: 6
+num_workers: 8
+save_interval: 20
+test_interval: 10
+
+# loss
+lambda1: 0.9
+lambda2: 0.95
+lambda2: 0.2
